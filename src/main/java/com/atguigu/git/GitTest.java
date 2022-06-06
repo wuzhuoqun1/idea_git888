88ba79efcee360d6641d725f8631351f2929cb7a
老吴 <wzq451467067@qq.com>
2022-06-06 18:37:47 +0800

    第5次更新

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -5,7 +5,8 @@ public class GitTest {
         System.out.println("第一次打印");
         System.out.println("第二次打印");
         System.out.println("第三次打印");
-        System.out.println("第三次打印");
+        System.out.println("第四次打印");
+        System.out.println("第五次打印");
 
 
     }
